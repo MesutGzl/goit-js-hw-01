6f48eea62d24c6aec4ca676b337e2dc4329b796e
Mesut Güzel <mesut.guzel@outlook.com.tr>
2025-02-15 22:49:08 +0300

    some issues fixed

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
-function makeTransaction(quantitiy, pricePerDroid) {
-  const totalPrice = quantitiy * pricePerDroid;
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
 
-  return `You ordered ${quantitiy} droids worth ${totalPrice} credits!`;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
